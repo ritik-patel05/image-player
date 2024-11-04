bf7ca4359aedc55081c56a6aad175c712932903b
Ritik Patel <ritikptl05@gmail.com>
2024-11-04 13:41:54 +0530

    feat: add logic to delete file in s3 and upload new file in Update API

--- internal/domain/service/image-service/update-image-metadata.go
+++ internal/domain/service/image-service/update-image-metadata.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/ritik-patel05/image-player/internal/domain/entity"
 	image_db "github.com/ritik-patel05/image-player/internal/infrastructure/db/image-repo"
+	image_s3 "github.com/ritik-patel05/image-player/internal/infrastructure/s3/image-repo"
 	"github.com/ritik-patel05/image-player/internal/models/dto"
 )
 
@@ -24,6 +25,24 @@ func UpdateImageMetadata(ctx context.Context, reqDTO dto.UpdateImageMetadata) er
 	err := image_db.UpdateImageMetadata(ctx, imgMeta)
 	if err != nil {
 		log.Printf("error updating image meta %v", err)
+		return err
+	}
+
+	newImgMeta, err := image_db.GetImageMetadata(ctx, imgMeta.ImageID)
+	if err != nil {
+		log.Printf("error while getting new image meta %v", err)
+		return err
+	}
+
+	err = image_s3.DeleteImageMetaData(ctx, imgMeta.ImageID)
+	if err != nil {
+		log.Printf("error deleting image meta from S3 %v", err)
+		return err
+	}
+
+	err = image_s3.UploadImageMetadata(ctx, *newImgMeta)
+	if err != nil {
+		log.Printf("error inserting image meta to S3 %v", err)
 		return err
 	}
 
